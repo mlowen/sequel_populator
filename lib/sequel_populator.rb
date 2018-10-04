ba744af3889094175672f536ec900497a1f7d64f
Mike Lowen <mike@mlowen.com>
2018-10-04 20:46:54 +1300

    Merged into a single file
    Forgot about the version constant when changing populator to a class. Added the version constant to the class, didn't make sense to have multiple files anymore so merged them all down into one.

--- lib/sequel_populator.rb
+++ lib/sequel_populator.rb
@@ -1,5 +1,76 @@
 # frozen_string_literal: true
 
 require 'sequel'
-require 'sequel_populator/run'
-require 'sequel_populator/version'
+
+require 'json'
+require 'yaml'
+
+module Sequel
+  # This class is responsible for the runner to populate the database with
+  # seed/dev/test data
+  class Populator
+    VERSION = '0.1.0'
+
+    def initialize(database, source)
+      Sequel.extension :inflector
+
+      @database = database
+      @source = source
+    end
+
+    def run
+      @database.transaction do
+        seed_data.each { |table, entities| process_table(table, entities) }
+      end
+    end
+
+    def self.run(database, source)
+      new(database, source).run
+    end
+
+    private
+
+    def process_table(table, entities)
+      table_sym = table.to_sym
+
+      entities.each do |entity|
+        fields = entity.reject do |field|
+          field.start_with?('$')
+        end.merge(fetch_references(entity))
+
+        create_unless_exists(table_sym, fields)
+      end
+    end
+
+    def fetch_references(entity)
+      references = {}
+
+      if entity.key?('$refs')
+        entity['$refs'].each do |ref, ref_fields|
+          id = @database[ref.pluralize.to_sym].first!(ref_fields)[:id]
+          references[ref.foreign_key.to_sym] = id
+        end
+      end
+
+      references
+    end
+
+    def create_unless_exists(table, fields)
+      existing = @database[table].first(fields)
+      @database[table].insert(fields) if existing.nil?
+    end
+
+    def seed_data
+      return @source if @source.is_a?(Hash)
+
+      case File.extname(@source).downcase
+      when '.json'
+        JSON.parse File.read(@source)
+      when '.yml'
+        YAML.safe_load File.read(@source)
+      else
+        raise 'Unable to handle source'
+      end
+    end
+  end
+end
