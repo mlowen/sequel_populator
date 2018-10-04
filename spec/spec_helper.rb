e79838b4b7809b82472591dec58e6ab09ec2516d
Mike Lowen <mike@mlowen.com>
2018-10-04 20:41:35 +1300

    Setting up unit testing
    Started to set up rspec to do testing, also had an attempt at setting up the travis-ci configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'simplecov'
+
+SimpleCov.start do
+  minimum_coverage 90
+
+  add_filter '/spec/'
+end
+
 require 'sequel_populator'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
-
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
   end
 end
